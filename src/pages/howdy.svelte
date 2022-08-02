2464dcaf0d1c86b9dd7ef65de20f7608a65823b8
Matthew Alicea <matthew.ivan.alicea@gmail.com>
2022-08-01 21:30:32 -0400

    testing src values

--- src/pages/howdy.svelte
+++ src/pages/howdy.svelte
@@ -1,14 +1,16 @@
 <script>
   import Cayo from 'cayo/component';
   // console.log('howdy', Cool);
+  const str = "../components/some/Cool.cayo.svelte";
 </script>
 
 howdyyy
 <br><a href="/">Back</a>
 <Cayo src="../components/Cool.cayo.svelte" />
-<Cayo src={"../components/some/Cool.cayo.svelte"} />
-<Cayo src={"../components/some/Cool.cayo.svelte"} />
+<Cayo src={str} />
+<Cayo okay={"str"} />
+<!-- <Cayo src={"../components/some/Cool.cayo.svelte"} /> -->
 
-<slot name="entry">
+<!-- <slot name="entry">
   <script src="/main2.js" data-cayo-entry />
-</slot>
+</slot> -->
